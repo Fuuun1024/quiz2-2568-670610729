38570c58de4fa6c0b8ce40d25d46026732e9099b
Fuuun1024 <worapopfong@gmail.com>
2025-09-08 11:48:00 +0700

    comment2

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,45 +1,45 @@
-import { useState } from "react";
-import {
-  Modal,
-  TextInput,
-  NumberInput,
-  Select,
-  Button,
-  Stack,
-} from "@mantine/core";
+// import { useState } from "react";
+// import {
+//   Modal,
+//   TextInput,
+//   NumberInput,
+//   Select,
+//   Button,
+//   Stack,
+// } from "@mantine/core";
 
-import { useDisclosure } from '@mantine/hooks';
+// import { useDisclosure } from '@mantine/hooks';
 
-type AddExpenseModalProps = {
-  opened: boolean;
-  onClose: () => void;
-  onAdd: (
-    name: string, 
-    amount: number | string, 
-    category: string
-  ) => void;
-};
+// type AddExpenseModalProps = {
+//   opened: boolean;
+//   onClose: () => void;
+//   onAdd: (
+//     name: string, 
+//     amount: number | string, 
+//     category: string
+//   ) => void;
+// };
 
-export default function AddExpenseModal({}: AddExpenseModalProps) {
-  const [name, setName] = useState<string>("");
-  const [amount, setAmount] = useState<string | number>(0);
-  const [category, setCategory] = useState<string | null>(null);
+// export default function AddExpenseModal({}: AddExpenseModalProps) {
+//   const [name, setName] = useState<string>("");
+//   const [amount, setAmount] = useState<string | number>(0);
+//   const [category, setCategory] = useState<string | null>(null);
 
-  const handleSubmit = () => {};
+//   const handleSubmit = () => {};
 
-  // หากต้องการแปง type string เป็น type number สามารถดูตัวอย่างนี้ได้
-  let val_number: number = Number("500.0");
-  console.log(val_number + 100); // 600.0
+//   // หากต้องการแปง type string เป็น type number สามารถดูตัวอย่างนี้ได้
+//   let val_number: number = Number("500.0");
+//   console.log(val_number + 100); // 600.0
 
 
-  const [opened, { open, close }] = useDisclosure(false);
+//   const [opened, { open, close }] = useDisclosure(false);
   
-  return (
-    /* Type additional text here. */
+//   return (
+//     /* Type additional text here. */
 
-      <Modal opened={opened} onClose={close} title="Authentication" centered>
-        {/* Modal content */}
-      </Modal>
+//       <Modal opened={opened} onClose={close} title="Authentication" centered>
+//         {/* Modal content */}
+//       </Modal>
 
-  );
-}
+//   );
+// }
